39f1ac8899e26b89de4bfab9108b084eab246c0d
anjastadlhofer <anja.stadlhofer@gmail.com>
2019-10-29 18:14:37 +0100

    create model

--- hw1/hw1/yamod/admin.py
+++ hw1/hw1/yamod/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
+from hw1.yamod.models import Song, Country, Person
 
 # Register your models here.
+
+class SongAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Song, SongAdmin)
+
+class CountryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Country, CountryAdmin)
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Person, PersonAdmin)
