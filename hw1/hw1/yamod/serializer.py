bbb6ee4ca5829d9ada78b0e13ad975792147bec7
anjastadlhofer <anja.stadlhofer@gmail.com>
2019-11-05 12:41:17 +0100

    CRUD

--- hw1/hw1/yamod/serializer.py
+++ hw1/hw1/yamod/serializer.py
@@ -1,7 +1,17 @@
-from hw1.yamod.models import Country
+from hw1.yamod.models import Country, Song, Person
 from rest_framework import serializers
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country 
         fields = ['name']
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song 
+        fields = ['title', 'genre', 'release_date', 'country', 'singers']
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person 
+        fields = ['first_name', 'last_name', 'year_of_birth']
--- hw1/hw1/yamod/urls.py
+++ hw1/hw1/yamod/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('countries/', views.country_list),
-    path('countries/<int:pk>/', views.country_detail)
+    path('countries/<int:pk>/', views.country_detail),
+    path('songs/', views.song_list),
+    path('songs/<int:pk>/', views.song_detail),
+    path('person/', views.person_list),
+    path('person/<int:pk>/', views.person_detail)
 ]
